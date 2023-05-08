ee6cb91f2e6ce1967618e7b303fbc9930e634d2d
hilosiva <sbthrnr@icloud.com>
2023-05-08 21:03:21 +0900

    UPDATE: 2023/05/08

--- chapter04/login/receive.php
+++ chapter04/login/receive.php
@@ -6,6 +6,21 @@
 </head>
 <body>
 <?php
+// ログインできるメルアドとパスワードを決める
+$email = "shibatest@icloud.com";
+$password = "shibashibashiba";
+
+// ログインチェック
+if (
+   $_POST['your_email'] ===  $email &&
+   $_POST['your_password'] === $password
+   ) {
+  // ログイン成功
+  echo '<p>ログイン成功</p>';
+} else {
+  // ログイン失敗
+  echo 'メールアドレスかパスワードが間違っています';
+}
 
 ?>
 </body>
--- chapter04/validate/receive.php
+++ chapter04/validate/receive.php
@@ -6,14 +6,36 @@
 </head>
 <body>
 <?php
+  // エラーメッセージ格納用の変数を初期化
+  $error = '';
+
   // 名前の必須項目チェック
   if ( $_POST['your_name'] === '') {
-    echo '<p>名前はちゃんと入力してな！</p>';
+    // echo '<p>名前はちゃんと入力してな！</p>';
+    $error .= '<p>名前はちゃんと入力してな！</p>';
   }
 
   // メルアドの形式チェック
-  if( filter_var($_POST['your_email'], FILTER_VALIDATE_EMAIL) === false ) {
-    echo '<p>メルアドの形式で入力してな！</p>';
+  if( !filter_var($_POST['your_email'], FILTER_VALIDATE_EMAIL) ) {
+    $error .= '<p>メルアドの形式で入力してな！</p>';
+  }
+
+  // パスワードの文字数チェック
+  if( mb_strlen( $_POST['your_password'] ) < 4 ) {
+    $error .= '<p>パスワードは4文字以上で！！</p>';
+  }
+
+
+  // エラーがあるかないか
+  if ( !empty($error)) {
+    // エラーがある
+
+    // エラーメッセージを画面に出す
+    echo $error;
+  } else {
+    // エラーがない
+    // 「エラーなし」って画面に出す
+    echo 'エラーなし';
   }
 ?>
 </body>
