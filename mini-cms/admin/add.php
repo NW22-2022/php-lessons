663ddfab89da1c452b387ad27a27d1f0b64bde6b
hilosiva <sbthrnr@icloud.com>
2023-06-16 20:02:23 +0900

    UPDATE: add.phpに処理を追加

--- mini-cms/admin/add.php
+++ mini-cms/admin/add.php
@@ -1,25 +1,38 @@
 <?php
-  // ファイルの読み込み（DBの設定ファイル、関数定義ファイル）
+// ファイルの読み込み（DBの設定ファイル、関数定義ファイル）
+require_once('../inc/config.php');
+require_once('../inc/functions.php');
 
-  try {
-    // DBの接続
+try {
+  // DBの接続
+  $dbh = new PDO(DSN, DB_USER, DB_PASSWORD);
 
-    // SQLのエラーがあった時例外を投げる設定
+  // SQLのエラーがあった時例外を投げる設定
+  $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // SQL文の作成（レコードの挿入：変数を埋め込むところは「?」をを入れておく）
+  // SQL文の作成（レコードの挿入：変数を埋め込むところは「?」をを入れておく）
+  $sql = 'insert posts(title, content, category_id, created)
+    values(?, ?, ?, now())';
 
-    // ステートメントの作成
+  // ステートメントの作成
+  $stmt = $dbh->prepare($sql);
 
-    // 「?」に値をガッチャンコ（?の数だけ記述）
+  // 「?」に値をガッチャンコ（?の数だけ記述）
+  $stmt->bindValue(1, $_POST['title'], PDO::PARAM_STR);
+  $stmt->bindValue(2, $_POST['content'], PDO::PARAM_STR);
+  $stmt->bindValue(3, (int)$_POST['category_id'], PDO::PARAM_INT);
 
-    // ステートメントを実行
+  // ステートメントを実行
+  $stmt->execute();
 
-    // DBの切断
-
-  } catch(  ) {
-    // 例外があった時
-    // エラーメッセージを画面に出して処理終了
-  }
+  // DBの切断
+  $dbh = null;
+} catch (PDOException $e) {
+  // 例外があった時
+  // エラーメッセージを画面に出して処理終了
+  echo 'エラー' . h($e->getMessage());
+  exit;
+}
 ?>
 <!DOCTYPE html>
 <html lang="ja">
